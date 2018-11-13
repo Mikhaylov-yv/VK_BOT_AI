8fa18f500892afb68116e8e3b9f8edf630340350
dezmond22 <dezmond22@yandex.ru>
2018-11-13 22:17:37 +0300

    1

---
+++ data/Message_loading.py
@@ -0,0 +1,2 @@
+import vkapi
+import os
---
+++ data/settings.py
@@ -0,0 +1,12 @@
+api_v = '5.8'
+token = ('',
+		)
+my_id = '255960' 
+deep = 2
+broker = {'user': '', 'password':'', 'ip':'', 'vhost':'vk_friends'}
+backend = broker
+m_friends_dct = {'file': 'mutual_friends_dct', 'graph':'mutual_friends_g'}
+d_friends_dct = {'file': 'deep_friends_dct', 'graph': 'deep_friends_g'}
+max_workers = 500
+delay = 1
+deep = 10
