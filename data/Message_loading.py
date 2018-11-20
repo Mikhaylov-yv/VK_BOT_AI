fb4a36012d1b9fa1c8f92464ae2f5f2ff7df12b2
dezmond22 <dezmond22@yandex.ru>
2018-11-20 23:59:22 +0300

    1

--- data/Message_loading.py
+++ data/Message_loading.py
@@ -5,99 +5,54 @@ import csv
 import time
 import datetime
 import numpy as np
+import re
 from my_data import MyVKData_O
 v=5.8
 session = vk.AuthSession(app_id=MyVKData_O.MY_PRIL_ID, user_login=MyVKData_O.LOGIN, user_password=MyVKData_O.GET_PASSWORD, scope='messages')
 vkapi = vk.API(session)
 api = vk.API(session, v=5.8)
-dialog_id = []
-dialog_type = []
-count_messag = []
-name = []
-sex = []
-bdate = []
+
+
+id = '55370581'
+time.sleep(1 / 3)
+data = vkapi.users.get(user_id=id, v=v)
+data1 = data[0]['first_name']
+data2 = data[0]['last_name']
+name = data1 + ' ' + data2
 us_id = []
-
-dialog_count = vkapi.messages.getConversations(v=v)['count']
-time.sleep(1/3)
-print(dialog_count//200)
+messag = []
+messag_date = []
+message = vkapi.messages.getHistory(user_id = id,count =200, v=v)
+message_count = message['count']
+print(message)
 count = 200
-for i in range((dialog_count//count)+1):
-    all_dialog= vkapi.messages.getConversations(v=v, count = count,offset = (i*count))#['items'][0]['conversation']['peer']
-    #count = dialog_count - i
-    #print(all_dialog)
-    time.sleep(1/3)
-    if i >= dialog_count//count:
-        count = dialog_count - count*i
-    for i in range(count):
-        data = all_dialog['items'][i]['conversation']['peer']['id']
-        dialog_id.append(data)
-        data = all_dialog['items'][i]['conversation']['peer']['type']
-        dialog_type.append(data)
-dialog_id = np.array(dialog_id)
-dialog_type = np.array(dialog_type)
-#d = dict(zip(dialog_type,dialog_id))
-#print(d)
-#print(d.get('user'))
-dialog = np.vstack((dialog_id, dialog_type))
-print(dialog)
-df1 = pd.DataFrame(dialog_id, columns=['id'])
-df2 = pd.DataFrame(dialog_type, columns=['dialog_type'])
-df = pd.concat([df1, df2], axis=1)
-df = df[df['dialog_type'] == 'user']
-id = np.array(df['id'].tolist())
-#print(df)
-#print(id) # список ид пользователь с диалогами у меня
-count_id=id.size
-time.sleep(1/3)
-for i in range(count_id):
-    time.sleep(1/3)
-    data= int(vkapi.messages.getHistory(user_id = id[i],v=v)['count'])
-    count_messag.append(data)
-    print(count_messag)
+
+def messagesget(i,message):
+    data = message['items'][i]['body']
+    reg = re.compile('[^а-яА-я1-9 .!,?:;]')
+    data = reg.sub('', data)
+    if bool(re.search('[а-яА-я]', message['items'][i2]['body'])):
+        messag.append(data)
+        data = message['items'][i]['date']
+        data = datetime.datetime.fromtimestamp(data)
+        messag_date.append(data.strftime('%d-%m-%Y %H:%M:%S'))
+        data = message['items'][i]['from_id']
+        us_id.append(data)
+
+for i in range((message_count//count)+1):
     time.sleep(1 / 3)
-    data = vkapi.users.get(user_id=id[i],fields=('bdate','sex'), v=v)
-    print(data)
-    data1 = data[0]['first_name']
-    #print(data)
-    data2 = data[0]['last_name']
-    #print(data)
-    data3 = data1 + ' ' + data2
+    message = vkapi.messages.getHistory(user_id=id,offset=i*count, count=count, v=v)
+    for i2 in range(len(message['items'])):
+        messagesget(i2, message)
 
-    print (type(data3))
-    name.append(data3)
-    data4 = data[0]['id']
-    us_id.append(data4)
-    print (us_id)
-    if 'sex' in data[0]:
-        data1 = data[0]['sex']
-    else:
-        data1 = ''
-    sex.append(data1)
-    if 'bdate' in data[0]:
-        data2 = data[0]['bdate']
-    else:
-        data2 = ''
-    bdate.append(data2)
-'''
-df1 = pd.DataFrame(us_id, columns=['us_id'])
-df2 = pd.DataFrame(name, columns=['name'])
-df3 = pd.DataFrame(count_messag, columns=['count_messag'])
-df4 = pd.DataFrame(sex, columns=['sex'])
-df5 = pd.DataFrame(bdate, columns=['bdate'])
-print (df4)
-df = pd.concat([df1, df2, df3, df4, df5], axis=1)
-print (df)
 
-FileName =  'Dialog_data.csv'
-myFile = open(FileName, 'w',newline='')
-df.to_csv(FileName, sep = ';')
-'''
-c = np.column_stack((us_id, name, count_messag,sex,bdate))
-#print(c)
-FileName =  'Dialog_data.csv'
-myFile = open(FileName, 'w',newline='')
+
+# Запись файла
+
+c = np.column_stack((us_id, messag, messag_date))
+print (c)
+FileName = name + ' переписка.csv'
+myFile = open(FileName, 'w', newline='')
 with myFile:
-    writer = csv.writer(myFile,delimiter=';')
+    writer = csv.writer(myFile, delimiter=';')
     writer.writerows(c)
-
